4563e2fbd8ce5d453fc7761ed46d124286878a0f
João Pedro <jpleite948@gmail.com>
2025-09-01 19:25:11 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+list = [
+    'jose', 'joao', "carla", 'pedro', 'carlos'
+]
+print("O Primeiro elemneto da lista: ")
+print(list[0])
+print("O Terceiro elemneto da lista: ")
+print(list[2])
+print("O Ultimo elemneto da lista: ")
+print(list[-1])
+
+if ('carla' in list ):
+    print("Carla esta na lista")
+else:
+    print("Carla nao esta na lista")
